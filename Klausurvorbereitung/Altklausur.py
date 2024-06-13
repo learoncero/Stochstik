15f2536ef454713b83468117b7eb82d5195abaee
Lea Roncero PC <lea.roncero@students.fhv.at>
2024-06-13 08:42:12 +0200

    added Altklausur

---
+++ Klausurvorbereitung/Altklausur.py
@@ -0,0 +1,57 @@
+from scipy.stats import hypergeom, t, chi2, norm
+import statistics
+import math
+
+# Aufgabe 4
+N = 60
+M = 10
+n = 3
+
+a1 = hypergeom.pmf(1, 60, 10, 3)
+a2 = hypergeom.pmf(2, 60, 10, 3)
+a3 = hypergeom.pmf(3, 60, 10, 3)
+
+print(f"4) ", a1 + a2 + a3)
+
+# Aufgabe 6
+values = [124, 145, 112, 124, 136, 129, 125, 131, 142, 114]
+n = len(values)
+
+mean = statistics.mean(values)
+s2 = statistics.variance(values)
+s = math.sqrt(s2)
+
+# Aufgabe 6
+print(f"6) mean: ", mean) 
+print(f"6) s2: ", s2) 
+print(f"6) s: ", s) 
+
+t_val = t.ppf(0.95, 9)
+print(f"6) t_val: ", t_val) 
+
+lower = mean - t_val * (s/math.sqrt(10))
+upper = mean + t_val * (s/math.sqrt(10))
+print(f"6) a) lower: ", lower) 
+print(f"6) a) upper: ", upper) 
+
+chi_val1 = chi2.ppf(0.95, 9)
+chi_val2 = chi2.ppf(0.05, 9)
+print(f"6) b) chi_val1: ", chi_val1) 
+print(f"6) b) chi_val2: ", chi_val2) 
+
+lower1 = (9 * s2)/chi_val1
+upper1 = (9 * s2)/chi_val2
+print(f"6) b) lower1: ", lower1) 
+print(f"6) b) upper1: ", upper1) 
+
+# Aufgabe 7
+mu0 = 15
+sigma = 0.04
+n = 16
+mean = 14.97
+alpha = 0.05
+
+z_val = ((mean - mu0)/(sigma/math.sqrt(n)))
+print(f"7) z_val", z_val)
+z_quan = norm.ppf(1-alpha)
+print(f"7) z_quan", z_quan)
--- Klausurvorbereitung/Probeklausur.py
+++ Klausurvorbereitung/Probeklausur.py
@@ -1,42 +1,44 @@
 from scipy.stats import poisson, expon, t
 import numpy as np
 import math
+import statistics
 
-a = poisson.cdf(4, 1)
-b = poisson.cdf(1, 1)
-
-print(a)
-print(b)
-print(a - b)
+a1 = poisson.pmf(2, 1)
+a2 = poisson.pmf(3, 1)
+a3 = poisson.pmf(4, 1)
+print(f"a1", a1)
+print(f"a2", a2)
+print(f"a3",a3)
+print(f"a1 + a2 + a3", a1 + a2 + a3)
 
 c = expon.cdf(0.75 * 3)
-print(c)
+print(f"c", c)
 d = expon.ppf(0.95, scale=1/3)
-print(d*60)
+print(f"d*60", d*60)
 
 data = [2.81, 2.62, 2.96, 3.03, 3.23, 3.31, 2.82, 3.21, 2.84, 3.20]
 e = np.mean(data)
-f = np.var(data)
-print(e)
-print(f)
+f = statistics.variance(data)
+print(f"e", e)
+print(f"f", f)
 
 t_val = t.ppf(0.975, 99)
 print(t_val)
 lower = (3 - (t_val * (math.sqrt(0.09)/10)))
 upper = (3 + (t_val * (math.sqrt(0.09)/10)))
-print(lower)
-print(upper)
+print(f"lower", lower)
+print(f"upper", upper)
 
 values = [101, 98, 96, 102,99, 101, 102, 103, 96, 97]
 mean = np.mean(values)
 var = np.var(values)
 std_dev = math.sqrt(var)
 
-print(mean)
-print(var)
-print(std_dev)
+print(f"mean", mean)
+print(f"var", var)
+print(f"std_dev", std_dev)
 
 t_val = ((mean-100)/(std_dev/math.sqrt(10)))
-print(t_val)
+print(f"t_val", t_val)
 crit_val = t.ppf(0.95, 9)
-print(crit_val)
+print(f"crit_val", crit_val)
